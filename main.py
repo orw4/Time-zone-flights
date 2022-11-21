2e3612ac3f6bdf6469ec440f109c0854204eae02
orw4 <76228002+orw4@users.noreply.github.com>
2022-11-21 19:38:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 # returns the other hour (or gets the take off and landing hours and returns the flight length)
 # 13/07/2022
 
-#TODO
-#find the time zones in the internet instead of asking
+#TODO find the time zones in the internet instead of asking
 
 def main():
     question = input("What are you looking for? the length of the flight, the take off hour or the landing hour? ")
@@ -17,89 +16,79 @@ def main():
     timezone2 = timezone(country2)
     if "off" in question:
         h = takeoff(timezone1, timezone2)
-        h = hourDescription(h)
         print("The take off is at", h)
     elif "land" in question:
         h = landing(timezone1, timezone2)
-        h = hourDescription(h)
         print("The landing is at", h)
     else:
         h = length(timezone1, timezone2)
-        h = minute(h)
-        print("The length of the flight is", h[0], ":", int(h[1]))
+        print("The length of the flight is", h)
 
 # Find the time zone of a country
 def timezone(country):
-    timezone0 = int(input("What time zone is there? ")) # make it look for the time zones in the internet
+    timezone0 = int(input("What time zone is there? ")) # TODO: make it look for the time zones in the internet
     return timezone0
 
+# Computes the take off hour
+def takeoff(timezone1, timezone2):
+    flightLength = int(input("How many hours is the flight? "))
+    min = int(input("And how many minutes (excluding the full hours)? "))
+    flightLength += min / 60
+    print("When do you land according to the second timezone (", timezone2, ")? (HH:MM) the hour is between 00:00 to 23:59")
+    landingHour = hourToNum(input(""))
+    takeoffHour = landingHour - timezone2 - flightLength + timezone1
+    takeoffHour = numToValidHour(takeoffHour)
+    return takeoffHour
+
+# Computes the landing hour
+def landing(timezone1, timezone2):
+    flightLength = int(input("How many hours is the flight? "))
+    min = int(input("And how many minutes (excluding the full hours)? "))
+    flightLength += min / 60
+    print("When do you take off according to the first timezone (", timezone1, ")? (HH:MM) the hour is between 00:00 to 23:59")
+    takeoffHour = hourToNum(input(""))
+    landingHour = takeoffHour - timezone1 + flightLength + timezone2
+    landingHour = numToValidHour(landingHour)
+    return landingHour
+
+# Computes the length of the flight
+def length(timezone1, timezone2):
+    print("When do you take off according to the first timezone (", timezone1, ")? (HH:MM) the hour is between 00:00 to 23:59")
+    takeoffHour = hourToNum(input(""))
+    print("When do you land according to the second timezone (", timezone2, ")? (HH:MM) the hour is between 00:00 to 23:59")
+    landingHour = hourToNum(input(""))
+    sameDay = input("Do you land on the same day you take off according to the date? ")
+    if not("es" in sameDay):
+        sameDay = input("Do you land on the day after you take off? ")
+        if "es" in sameDay:
+            landingHour += 24
+        else:
+            landingHour -= 24
+    flightLength = landingHour - timezone2 - takeoffHour + timezone1
+    flightLength = numToHour(flightLength)
+    return flightLength
+
+
 # Gets a string hour and returns it as a number
-def hour(str):
+def hourToNum(str):
     num = int(str[0]) * 10 + int(str[1])
     num = num + (int(str[3]) * 10 + int(str[4])) / 60
     return num
 
-# Computes the take off hour
-def takeoff(timezone1, timezone2):
-    length = hour(input("How long is the flight? (HH:MM) "))
-    landing = hour(input("When do you land? (HH:MM) As am/pm " ))
-    landing = hourToNum(landing)
-    h = landing - length
-    h = h + timezone1 - timezone2
-    return h
+# Gets a number hour and returns it as a string hour between 00:00 and 23:59
+def numToValidHour(num):
+    while num < 0:
+        num += 24
+    while num >= 24:
+        num -= 24
+    return numToHour(num)
 
-# Computes the landing hour
-def landing(timezone1, timezone2):
-    length = hour(input("How long is the flight? (HH:MM) "))
-    takeoff = hour(input("When do you take off? (HH:MM) As am/pm "))
-    takeoff = hourToNum(takeoff)
-    h = takeoff + length
-    h = h + timezone2 - timezone1
-    return h
-
-# Computes the length of the flight
-def length(timezone1, timezone2):
-    takeoff = hour(input("When do you take off? (HH:MM) As am/pm "))
-    takeoff = hourToNum(takeoff)
-    landing = hour(input("When do you land? "))
-    landing = hourToNum(landing)
-    landing = landing + timezone1 - timezone2
-    h = landing - takeoff
-    return h
-
-# Gets a number representing a hour and returns the hour with number of minutes
-def minute(hour):
-    hours = int(hour // 1)
-    minutes = int((hour % 1) * 60)
-    return [hours, minutes]
-
-def hourToNum(hour):
-    am = input("Is it am or pm? ")
-
-# Gets a number representing a hour and returns a valid hour with am/pm
-# Returns the hour, the number of days passed (a day before/a day after/other), and whether it is am (or pm)
-def hourDescription(h):
-    days = 0
-    am = True
-    while h > 24:
-        days = days + 1
-        h = h - 24
-    while h < 0:
-        h = h + 24
-        days = days - 1
-    if h > 12:
-        h = h -12
-        am = False
-    h = minute(h)
-    h = str(h[0]) + ":" + str(h[1])
-    if am == True:
-        h = h + "am"
-    else:
-        h = h + "pm"
-    if days > 0:
-        h = h + "," + str(days) + "days after"
-    elif days < 0:
-        h = h + "," + str(days) + "days after"
-    return h
+# Gets a number hour and returns it as a string
+def numToHour(num):
+    stri = ""
+    stri += str(int(num))
+    stri += ":"
+    stri += str(int((num - int(num)) * 60))
+    return stri
 
 main()
